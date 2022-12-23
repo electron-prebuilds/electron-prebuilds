a4e89cb804c423bb129b06d6b7f4ac7e9beab8a8
Ozan Akın <ozn.akn@gmail.com>
2022-12-24 02:57:12 +0300

    fix publish

--- src/publish.mjs
+++ src/publish.mjs
@@ -1,6 +1,12 @@
 export default async function publish(libData) {
-    libData.targetPath = path.join(process.cwd(), 'libs', name);
-    cd(libData.targetPath);
+    let { source } = libData;
+    let name = source.split('/').at(-1);
+    if (name.endsWith('.git')) {
+        name = name.slice(0, -4);
+    }
+
+    const targetPath = path.join(process.cwd(), 'libs', name);
+    cd(targetPath);
 
     await $`npm publish --access public --dry-run`;
 }
