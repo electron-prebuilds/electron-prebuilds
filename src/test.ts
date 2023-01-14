3cbdc659272be607174e41cee28fb86101ba953b
Ozan Akın <ozn.akn@gmail.com>
2023-01-14 20:19:16 +0300

    fix

--- src/test.ts
+++ src/test.ts
@@ -3,6 +3,8 @@ import 'zx/globals';
 import type { PackageContext } from './defs.js';
 
 export default async function test(ctx: PackageContext) {
+  if (ctx.libData.os && !ctx.libData.os.includes(process.platform)) return;
+
   if (!ctx.libData.test) return;
 
   cd(ctx.path);
